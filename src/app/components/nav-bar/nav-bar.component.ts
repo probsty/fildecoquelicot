99b81fd67fa8bb51e95f3e9e83809fb55f28ccf9
Probst <yann.probst@epitech.eu>
2025-02-05 10:51:29 +0100

    add toglle

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import {MenubarModule} from 'primeng/menubar';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    MenubarModule
+    MenubarModule,
+    NgClass
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
